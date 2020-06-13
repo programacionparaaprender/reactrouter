677665d5dba8b0addd8c9338b4f1418012764c2c
Luis Correa <yancel209@gmail.com>
2020-06-13 18:47:42 -0500

    funciona redux con persistencia de datos

--- ReactRedux/ClientApp/src/index.tsx
+++ ReactRedux/ClientApp/src/index.tsx
@@ -1,11 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.css';
-
+import { PersistGate } from 'redux-persist/integration/react'
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
 import { createBrowserHistory } from 'history';
-import configureStore from './store/configureStore';
+
+import configureStore from './store/configureStorePersisted';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
@@ -16,12 +18,23 @@ const history = createBrowserHistory({ basename: baseUrl });
 // Get the application-wide store instance, prepopulating with state from the server where available.
 const store = configureStore(history);
 
-ReactDOM.render(
-    <Provider store={store}>
+
+ 
+const App2 = () => {
+    return (
         <ConnectedRouter history={history}>
             <App />
         </ConnectedRouter>
-    </Provider>,
+    );
+  };
+
+ReactDOM.render(
+    <Provider store={store.store}>
+        <PersistGate loading={null} persistor={store.persistor}>
+          <App2 />
+        </PersistGate>
+      </Provider>
+    ,
     document.getElementById('root'));
 
 registerServiceWorker();
---
+++ ReactRedux/ClientApp/src/types.ts
@@ -0,0 +1,6 @@
+export interface Producto{
+    id: string;
+    name: string;
+    description:string
+    price: number;
+}
